f8680269e444d740540ed3e75f145bda15cc83de
NoteBoxz <139518687+NoteBoxz@users.noreply.github.com>
2024-11-18 12:01:08 -0500

    0.2.8

--- LethalMin/OnionFuseRules.cs
+++ LethalMin/OnionFuseRules.cs
@@ -33,7 +33,7 @@ namespace LethalMin
         [HideInInspector]
         public int FuseID;
         [HideInInspector]
-        public string version = "0.2.7";
+        public string version = "0.2.8";
 
         /// <summary>
         /// Checks if an onion can fuse with the given OnionType.
