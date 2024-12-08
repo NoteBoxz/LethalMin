f4ed9bc83a74d625ed1a308f4a755f386ad3e08e
NoteBoxz <139518687+NoteBoxz@users.noreply.github.com>
2024-12-08 12:34:58 -0500

    13

--- LethalMin/PikminType.cs
+++ LethalMin/PikminType.cs
@@ -198,7 +198,7 @@ namespace LethalMin
         [HideInInspector]
         public bool HasBeenRegistered;
         [HideInInspector]
-        public string version = "0.2.12";
+        public string version = "0.2.13";
     }
 
     public struct PikminMeshData
