94c7fec0ebb0c74becd335297a00ba3efa11de66
NoteBoxz <139518687+NoteBoxz@users.noreply.github.com>
2024-12-10 22:47:04 -0500

    whoopsies

--- LethalMin/Patches/MaskedPlayerEnemyPatch.cs
+++ LethalMin/Patches/MaskedPlayerEnemyPatch.cs
@@ -7,6 +7,7 @@ using LethalMin;
 using System.Collections;
 using System.Diagnostics;
 using Unity.Multiplayer.Tools.MetricTypes;
+using LethalMon.Behaviours;
 
 namespace LethalMin.Patches
 {
@@ -81,6 +82,7 @@ namespace LethalMin.Patches
             if (!LethalMin.PuffMask) { return; }
             if (__instance.isEnemyDead) { return; }
             if (!__instance.IsServer) { return; }
+            if (LethalMin.IsDependencyLoaded("LethalMon") && LETHALMON_ISTAMED(__instance)) { return; }
             if (LethalMin.FindNearestIdlePikmin(__instance.transform.position, LethalMin.MaskedWhistleRange, 1).Count > 0
              || LethalMin.FindNearestPuffmin(__instance.transform.position, LethalMin.MaskedWhistleRange, 1).Count > 0)
             {
@@ -91,6 +93,13 @@ namespace LethalMin.Patches
             {
                 __instance.GetComponentInChildren<PuffminOwnerManager>().DoThrow();
             }
+        }
+
+        public static bool LETHALMON_ISTAMED(MaskedPlayerEnemy __instance)
+        {
+            TamedEnemyBehaviour enemy = __instance.GetComponent<TamedEnemyBehaviour>();
+            if (enemy == null) return false;
+            return enemy.IsTamed;
         }
 
 
